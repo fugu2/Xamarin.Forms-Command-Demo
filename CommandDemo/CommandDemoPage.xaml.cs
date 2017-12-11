02bbfca62ede846534ff66b44f327c8badc79bbd
fugu2 <sacha.schwab@gmail.com>
2017-12-11 08:55:43 +0100

    Initial commit

---
+++ CommandDemo/CommandDemoPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace CommandDemo
+{
+    public partial class CommandDemoPage : ContentPage
+    {
+        public CommandDemoPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
